0d802a4157b85a716df1b965d488848df4cf3057
Vian Navalino <viansatria57@gmail.com>
2020-12-20 17:27:32 +0700

    first commit

---
+++ UASVirus/Assets/collisionScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //for this to work both need colliders, one must have rigid body (spaceship) the other must have is trigger checked.
+    void OnTriggerEnter(Collider col)
+    {
+        if (GameObject.FindGameObjectsWithTag("Player").Length == 0)
+        {
+
+            GameObject enemy = Instantiate(Resources.Load("virus2", typeof(GameObject))) as GameObject;
+            GameObject enemy1 = Instantiate(Resources.Load("virus2", typeof(GameObject))) as GameObject;
+            GameObject enemy2 = Instantiate(Resources.Load("virus2", typeof(GameObject))) as GameObject;
+            GameObject enemy3 = Instantiate(Resources.Load("virus2", typeof(GameObject))) as GameObject;
+
+        }
+        Destroy(gameObject);
+        Destroy(col.gameObject);
+    }
+
+}
